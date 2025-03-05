364620f61056eceaf51edb2fc1f08e07438e1b66
aaromerogg <46766552+aaromerogg@users.noreply.github.com>
2025-03-04 20:36:52 -0500

    Informacion Nutricional

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ if ingredients_list:
     ingredients_string = ''
     for i in ingredients_list:
         ingredients_string+=i+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
+        st.subheader(i+" Nutrition Information")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string +"""','"""+name_on_order+ """')"""
@@ -38,7 +41,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
